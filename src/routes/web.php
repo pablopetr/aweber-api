702e4d47df99f98c283d955a0783d627e6076213
Pablo Eliézer <opablopetr@gmail.com>
2023-01-08 20:36:21 -0300

    Remove unnecessary routes

--- src/routes/web.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Pablopetr\AweberApi\Controllers\AuthenticateController;
-
-Route::get('/aweber/callback', AuthenticateController::class)->name('aweber.callback');
